209a2c9a14ba200e6db3298a61fc7eec6463083c
Younus AL-Saadi <72101955+younus-alsaadi@users.noreply.github.com>
2023-01-11 13:34:16 +0100

    Update and rename main.py to Data_Cleaning.py

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,25 +1,25 @@
 import numpy as np
 import pandas as pd
 import warnings
-import seaborn as sns
-import matplotlib.pyplot as plt
-import nltk
 import re
 from sklearn.model_selection import train_test_split
+from bs4 import BeautifulSoup
+import spacy
+
 
 warnings.filterwarnings('ignore') # Hides warning
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
-# Data clean
+# Data cleaning
 
 missing_value = ["N/a", "na", np.nan, np.NAN, np.NaN, "null"]
-df = pd.read_table('sample_us.tsv', error_bad_lines=False, na_values=missing_value)
+df = pd.read_table('amazon_reviews_us_Electronics_v1_00.tsv', error_bad_lines=False, na_values=missing_value)
 
 df = df.dropna()
 data = df.copy()
 review_id = len(data["review_id"].unique())
-print("review_id: " + str(review_id))
+#print("review_id: " + str(review_id))
 
 # Visualizing the distributions of numerical variables:
 
@@ -31,7 +31,7 @@ print("review_id: " + str(review_id))
 # Train/Test Split
 training_data, testing_data = train_test_split(data, test_size=0.2, random_state=25)
 
-data.info()
+#data.info()
 
 # Normalization : 1- converting all the characters to lowercase
 
@@ -39,34 +39,56 @@ training_data['review_body'] = training_data['review_body'].str.lower()
 training_data['review_headline'] = training_data['review_headline'].str.lower()
 
 
+
 # Normalization : 2- converting all whitespace and punctuation into a single space to get rid of any inconsistencies.
-training_data['review_body'] = " ".join(training_data['review_body'].split())
-training_data['review_headline'] = " ".join(training_data['review_headline'].split())
+review_body = re.sub(' +', ' ', str(training_data['review_body']))
+review_headline = re.sub(' +', ' ', str(training_data['review_headline']))
 
-training_data['review_headline'] = re.sub(r"""
+
+
+review_body = re.sub(r"""
                [,.;@#?!&$]+  # Accept one or more copies of punctuation
                \ *           # plus zero or more copies of a space,
                """,
                " ",          # and replace it with a single space
-               training_data['review_body'], flags=re.VERBOSE)
-training_data['review_body'] = re.sub(r"""
+               str(training_data['review_body']), flags=re.VERBOSE)
+review_headline = re.sub(r"""
                [,.;@#?!&$]+  # Accept one or more copies of punctuation
                \ *           # plus zero or more copies of a space,
                """,
                " ",          # and replace it with a single space
-               training_data['review_headline'], flags=re.VERBOSE)
+               str(training_data['review_headline']), flags=re.VERBOSE)
 
-print(training_data['review_body'])
 
-#df=df.dropna()
-#data = df.copy()
-#print(data.isnull().sum())
+# Noise Removal: Removing HTML Tags (using BeautifulSoup’s)
 
-#print(data.info())
-#print(data["customer_id"].unique())
-#asins_unique = len(data["customer_id"].unique())
-#print("Number of customer_id: " + str(asins_unique))
+review_body = BeautifulSoup(review_body, "lxml").text
 
-#data.hist(bins=50, figsize=(20,15))
-#plt.show()
-#data["asins"].unique()
+
+
+# Noise Removal: Expanding Contractions
+
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    return phrase
+
+review_body = decontracted(review_body)
+
+
+print(len(review_body))
+print(review_body)
+
+
