3bfeb3b2247b9d2755bc66319847412652f4023f
Younus AL-Saadi <72101955+younus-alsaadi@users.noreply.github.com>
2023-01-16 17:20:22 +0100

    Update Data_Cleaning.py

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,13 +1,13 @@
-import os
 import numpy as np
 import pandas as pd
 import warnings
 import re
-import spacy
-
 from sklearn.model_selection import train_test_split
 from bs4 import BeautifulSoup
 
+import numpy as np
+
+import string
 
 warnings.filterwarnings('ignore') # Hides warning
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -17,10 +17,8 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 missing_value = ["N/a", "na", np.nan, np.NAN, np.NaN, "null"]
 df = pd.read_table('amazon_reviews_us_Electronics_v1_00.tsv', error_bad_lines=False, na_values=missing_value)
-
 df = df.dropna()
-data = df.copy()
-review_id = len(data["review_id"].unique())
+review_id = len(df["review_id"].unique())
 #print("review_id: " + str(review_id))
 
 # Visualizing the distributions of numerical variables:
@@ -30,40 +28,34 @@ review_id = len(data["review_id"].unique())
 
 
 
-# Train/Test Split
-training_data, testing_data = train_test_split(data, test_size=0.2, random_state=25)
+
 
 #data.info()
 
 # Normalization : 1- converting all the characters to lowercase
-training_data['review_body'] = training_data['review_body'].str.lower()
-training_data['review_headline'] = training_data['review_headline'].str.lower()
+
+df['review_body'] = df['review_body'].str.lower()
+
+
 
 
 
 # Normalization : 2- converting all whitespace and punctuation into a single space to get rid of any inconsistencies.
-review_body = re.sub(' +', ' ', str(training_data['review_body']))
-review_headline = re.sub(' +', ' ', str(training_data['review_headline']))
+
+
+for idx, x in enumerate(df['review_body']):
+    df['review_body'][idx] = " ".join(x.split())
+    df['review_body'][idx] = re.sub(r'[^\w\s]', '', x)
 
 
 
-review_body = re.sub(r"""
-               [,.;@#?!&$]+  # Accept one or more copies of punctuation
-               \ *           # plus zero or more copies of a space,
-               """,
-               " ",          # and replace it with a single space
-               str(training_data['review_body']), flags=re.VERBOSE)
-review_headline = re.sub(r"""
-               [,.;@#?!&$]+  # Accept one or more copies of punctuation
-               \ *           # plus zero or more copies of a space,
-               """,
-               " ",          # and replace it with a single space
-               str(training_data['review_headline']), flags=re.VERBOSE)
+
 
 
 # Noise Removal: Removing HTML Tags (using BeautifulSoup’s)
 
-review_body = BeautifulSoup(review_body, "lxml").text
+for idx, x in enumerate(df['review_body']):
+    df['review_body'][idx] = BeautifulSoup(x, "lxml").text
 
 
 
@@ -86,11 +78,15 @@ def decontracted(phrase):
     phrase = re.sub(r"\'s", " is", phrase)
     return phrase
 
-review_body = decontracted(review_body)
+for idx,x in enumerate(df['review_body']):
+    df['review_body'][idx] = decontracted(x)
 
 
-print(len(review_body))
-print(review_body)
+# Train/Test Split
+training_data, testing_data = train_test_split(df, test_size=0.2, random_state=25)
 
 
-# TODO: Either save cleaned dataset as new file or warp in function and return to model.py
+#df['review_body']=training_data['review_body']
+
+header = ["star_rating","review_body"]
+training_data.to_csv('output.csv', columns = header)
