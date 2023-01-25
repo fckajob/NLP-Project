62e58d8f2010b3d3de780d9c1ca3ad984ebf5d93
Kevin Kraus <hello@kevinkrs.com>
2023-01-25 11:32:42 +0100

    Seperated model from training

---
+++ eval.py
@@ -0,0 +1,26 @@
+import spacy
+import pandas as pd
+import json
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from model.model import ReviewModel
+
+nlp = spacy.load('./models/reviews_1_balanced_full')
+
+
+
+train_data = pd.read_csv('./data/train_balanced.csv')
+test_data = pd.read_csv('./data/testing.csv')
+test_data = test_data[:100]
+
+model = ReviewModel(train_data, test_data, spacy_model=None, evaluate_only=False)
+scores = model.evaluation(nlp)
+print(scores)
+
+json_object = json.dumps(scores, indent=4)
+
+with open("./eval/model_eval.json", "w") as f:
+    f.write(json_object)
